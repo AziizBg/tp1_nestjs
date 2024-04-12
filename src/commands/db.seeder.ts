958a34cda792a9e052328d8405f86ab0f8b3da9d
= <mohamedaziz.benghorbel@insat.ucar.tn>
2024-04-12 23:04:50 +0100

    relative paths

--- src/commands/db.seeder.ts
+++ src/commands/db.seeder.ts
@@ -15,8 +15,17 @@ async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   const cvService = app.get(CvService);
   const userService = app.get(UserService);
-  //   const users = await userService.findAll({role: UserRoleEnum.USER});
+  // const users = await userService.findAll({role: UserRoleEnum.USER});
   const users = userService.findAll();
+
+  const user = await userService.create({
+    email: 'example@example.com',
+    password: 'password',
+    username: 'user',
+  });
+
+
+
   for (let i = 1; i < 10; i++) {
     const newCv = new CV();
     newCv.firstname = randFirstName();
@@ -24,12 +33,10 @@ async function bootstrap() {
     newCv.age = Math.floor(Math.random() * 100);
     newCv.cin = randNumber({ min: 10000000, max: 99999999 });
     newCv.job = randJobTitle();
-    // newCv.user= users[Math.floor(Math.random() * users.length)];
-
+    newCv.user= users[Math.floor(Math.random() * (await users).length)];
     newCv.path = '';
     console.log('the new cv to add', newCv);
-    // const user = users[Math.floor(Math.random() * users.length)];
-    // await cvService.create(newCv, user);
+    await cvService.create(newCv);
   }
   await app.close();
 }
